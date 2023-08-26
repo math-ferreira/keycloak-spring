8451a3883647e21e820c079c6c80d3673e929748
math-ferreira <mat.s.ferreira@gmail.com>
2023-08-26 12:58:22 +0100

    adding keycloak and config to auth

---
+++ src/main/java/com/example/keycloak/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.keycloak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .anyRequest()
+                                .authenticated()
+                ).oauth2ResourceServer(rs ->
+                        rs.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(jwtAuthConverter))
+                ).sessionManagement(c ->
+                        c.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).build();
+
+    }
+}
